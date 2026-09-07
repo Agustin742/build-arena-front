a4b62cf933089adfd6a22f5edb58abedbd581b7c
Agustin Tabarcache <agustintabarcache74@gmail.com>
2026-09-07 13:38:07 -0300

    feat(app): register the top command in the console

--- src/app/boot/game-commands.ts
+++ src/app/boot/game-commands.ts
@@ -2,6 +2,7 @@ import { type Command } from '@/shared/commands'
 
 import { authCommands } from './auth-commands'
 import { buildCommands } from './build-commands'
+import { leaderboardCommands } from './leaderboard-commands'
 import { skillCommands } from './skill-commands'
 
 /** Everything the console knows how to do. Scope decides which ones show up where. */
@@ -9,4 +10,5 @@ export const gameCommands: readonly Command[] = [
   ...authCommands,
   ...skillCommands,
   ...buildCommands,
+  ...leaderboardCommands,
 ]
